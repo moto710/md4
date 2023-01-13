427894687f8a905f7af40553171c1eba431f62a8
Minh Thien <thien.nhm92@gmail.com>
2023-01-13 23:51:54 +0700

    validate withdraw

---
+++ json-html-banking-customer/views/assets/js/appBase.js
@@ -0,0 +1,8 @@
+class AppBase {
+    static DOMAIN = "http://localhost:3000"
+
+    static API_CUSTOMER = this.DOMAIN + "/customers";
+    static API_DEPOSIT = this.DOMAIN + "/deposits";
+    static API_WITHDRAW = this.DOMAIN + "/withdraws";
+    static API_TRANSFER = this.DOMAIN + "/transfers";
+}
